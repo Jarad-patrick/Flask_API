02876f017194f6ec5829ab2e22ab6de20d2dba3b
Jarad-patrick <jaradpatrick350@gmail.com>
2025-12-08 19:14:28 +0100

    add logging to /get-user and /health endpoints for request tracking

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from flask import Flask, request, jsonify
+import logging
+
 
 app = Flask(__name__)
 
 
-#using a get method
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# using a GET method
 @app.route('/get-user/<user_id>')
 def get_user(user_id):
-    user_id = {
-        'user_id': user_id,         
+    logging.info(f"Request received for user_id: {user_id}")
+    user_data = {
+        'user_id': user_id,
         'name': 'john patrick',
         'email': 'johnpatrick@gmail.com'
     }
-
+    return jsonify(user_data)
 
 @app.get("/health")
 def health_check():
+    logging.info("Health check endpoint called")
     return {"status": "ok", "message": "API is running"}
 
-
-
-if __name__ == '__main__' :
-    app.run(debug=True)  #run the flask server 
+if __name__ == '__main__':
+    app.run(debug=True)
